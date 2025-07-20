6fc31eaf571f252f33370b98ed9f2a6c5e8c978b
yhkim <148407209+honeyprofit@users.noreply.github.com>
2025-07-20 23:24:19 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 // Importing module
-import { addToCart } from './shoppingCart.js';
-console.log('Importing module');
+// import { addToCart, totalPrice as price, tq } from './shoppingCart.js';
+// addToCart('bread', 5);
+// console.log(price, tq);
 
+console.log('Importing module');
 // console.log(shippingCost);
 
-addToCart('bread', 5);
+import * as ShoppingCart from './shoppingCart.js';
+ShoppingCart.addToCart('bread', 5);
+console.log(ShoppingCart.totalPrice);
