89c7434debda4f08078b8042df981da7f21f5879
yhkim <148407209+honeyprofit@users.noreply.github.com>
2025-07-26 22:40:37 +0900

    Update shoppingCart.js

--- shoppingCart.js
+++ shoppingCart.js
@@ -2,7 +2,7 @@
 console.log('Exporting module');
 
 const shippingCost = 10;
-const cart = [];
+export const cart = [];
 
 export const addToCart = function (product, quantity) {
   cart.push({ product, quantity });
@@ -14,4 +14,8 @@ const totalQuantity = 23;
 
 export { totalPrice, totalQuantity as tq };
 
-// export default;
+// 이름이 없는 함수이므로 import시에 이름 정해줄 수 있음
+export default function (product, quantity) {
+  cart.push({ product, quantity });
+  console.log(`${quantity} ${product} added to the cart`);
+}
