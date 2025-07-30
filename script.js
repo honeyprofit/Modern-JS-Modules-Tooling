5168b17357a6ebfd0fb25cef1b63df717b6a9e26
yhkim <148407209+honeyprofit@users.noreply.github.com>
2025-07-30 21:30:40 +0900

    Update script.js

--- script.js
+++ script.js
@@ -17,3 +17,29 @@ add('bread', 5);
 add('apples', 4);
 
 console.log(cart);
+
+///////////////////////////////////////
+// Top-Level Await (ES2022)
+
+// console.log('Start fetching!');
+// const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+// const data = await res.json();
+// console.log(data);
+// console.log('End fetching');
+
+const getLastPost = async function () {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const data = await res.json();
+
+  return { title: data.at(-1).title };
+};
+
+const lastPost = getLastPost();
+console.log(lastPost); //promise
+
+// way1: Not very clean
+// lastPost.then(last => console.log(last));
+
+// way2: top level await
+const lastPost2 = await getLastPost();
+console.log(lastPost2);
